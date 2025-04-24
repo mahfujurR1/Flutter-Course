1cb433c8e3c68a2f7a9109b7f49316ff9b429db4
Mahfujur-Rahman <mahfujurrahman2077@gmail.com>
2025-04-25 04:53:26 +0600

    All about Dart String

---
+++ Strings/Strings.dart
@@ -0,0 +1,39 @@
+main()
+{
+  String name ='Mahfujur Rahman';
+  print(name);
+
+  //String Operations
+
+  //Concatation
+  String firstName = 'Mahfujur';
+  String lastName = 'Rahman';
+
+  String fullName = firstName + ' ' + lastName;
+  //print('FullName : $fullName');
+
+ //String interpolation
+  String City='Paris';
+  //print('one day I go to $City oneday');
+  /*
+  Let's consider an example to better comprehend this concept:
+  int appleCount = 5;
+  print('I have $appleCount apples.');
+  print('If I eat one, I will have ${appleCount - 1} apples.');
+  */
+
+  //String length
+  String subject= 'Computer Science of Engineering ';
+  //print(subject.length);
+
+  //Multi line String
+  String Bangladesh ='''
+Bangladesh is known for its stunning natural beauty and rich cultural heritage.
+Bangladesh's combination of natural beauty, cultural richness,
+and historical significance makes it a unique and beautiful destination.
+''';// we can also 3 double qoutes
+//print(Bangladesh);
+
+
+
+}
