ec6aadb76a0e07bd68caff5f310916e806492045
Mahfujur-Rahman <mahfujurrahman2077@gmail.com>
2025-04-25 04:59:31 +0600

    All about dart Boolean

---
+++ Boolean/Boolean.dart
@@ -0,0 +1,15 @@
+main()
+{
+  //THis is data type can hold only two values true- means correct,yes,on false-means no,incorrect,off
+  bool isSunny = true;
+  bool isRainy = false;
+  print('Is it sunny today? $isSunny');
+  print('Is it sunny today? $isRainy');
+
+
+  //example with condition
+  int age =18;
+
+  bool isAdult= age>=18;//true
+  print(isAdult);
+}
