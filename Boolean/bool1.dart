31456b330b9c959607195396a68eb3ea7fe55a23
Mahfujur-Rahman <mahfujurrahman2077@gmail.com>
2025-04-25 15:38:49 +0600

    Adding item on list

---
+++ Boolean/bool1.dart
@@ -0,0 +1,10 @@
+ main() {
+  bool isStudent = true;
+  bool hasLibraryCard = false;
+
+  if (isStudent && hasLibraryCard) {
+    print('Access granted to the library.');
+  } else {
+    print('Access denied.');
+  }
+}
---
+++ List/AddingInList.dart
@@ -0,0 +1,16 @@
+main()
+{
+  List<String>friuts =['Apple','Banana','Mango'];
+  List<int>number=[1,2,3,4];
+
+  print(friuts[2]);
+  print(number[2]);
+
+  // Adding items
+  friuts.add('Lemon'); //we can add one item to list by this 
+
+  friuts.addAll(['Orange','Pine Apple','Water Mellon']); //we can add all item in list by this
+
+  print(friuts[5]);
+
+}
