b43f5ecd388baaca6d71d67629d2c3f6cdda5b48
Mahfujur-Rahman <mahfujurrahman2077@gmail.com>
2025-04-25 16:32:48 +0600

    Add items on set

---
+++ Set/Add_Items_Set.dart
@@ -0,0 +1,16 @@
+main()
+{
+
+  Set<String>Countries ={'Bangladesh','China','China','Pakistan'};
+
+  print(Countries);
+ 
+  
+  //add items 
+
+  Countries.add('bhutan');
+  Countries.addAll({'India','Qatar','Qatar'});
+
+  print(Countries);
+  print(Countries.length);
+}
---
+++ Set/Set.dart
@@ -0,0 +1,21 @@
+main()
+{
+  //set is a collection of unique items no duplicates are allowed - Creating set: use {}
+  Set<String>Countries ={'Bangladesh','China','China','Pakistan'};
+
+  print(Countries);//output will be {Bangladesh, China, Pakistan}
+
+  var number ={1,2,3};
+  print(number);
+  
+  //add items 
+
+  Countries.add('bhutan');
+  Countries.addAll({'India','Qatar','Qatar'});
+
+  print(Countries);
+  print(Countries.length);
+
+
+
+}
