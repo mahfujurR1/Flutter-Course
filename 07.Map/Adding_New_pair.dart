6f3484709dd7efe5d5c05a12083a1b07ba66ee20
Mahfujur-Rahman <mahfujurrahman2077@gmail.com>
2025-04-25 17:18:47 +0600

    Adding new value on Map

---
+++ 07.Map/Adding_New_pair.dart
@@ -0,0 +1,15 @@
+main()
+{
+
+
+  Map<String,String>Capitals={
+    'Bangladesh': 'Dhaka\n',
+    'India' : 'new Delhi\n',
+    'Nepal' :'kathmandu\n'  //\n is a new line
+  };
+
+  //Adding a new key-value pair
+  Capitals['Bhutan']='Thimpu\n';
+  print(Capitals);
+
+}
