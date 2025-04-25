47489800d9078f091b04dacd11e9e1458a00ee66
Mahfujur-Rahman <mahfujurrahman2077@gmail.com>
2025-04-25 17:20:36 +0600

    Update Value on Map

---
+++ 07.Map/Update_value.dart
@@ -0,0 +1,14 @@
+main()
+{
+
+
+  Map<String,String>Capitals={
+    'Bangladesh': 'Dhaka\n',
+    'India' : 'new Delhi\n',
+    'Nepal' :'kathmandu\n'
+  };
+
+  //Update value
+  Capitals['Nepal']="Pokhra\n";
+  print(Capitals);
+}
